3c0e6627452f28eb52cf5d620dc5ee99bfb49aa0
Mark Davidson <mark@4each.co.uk>
2012-02-27 08:33:18 +0000

    Added some prelude config

--- personal/personal.el
+++ personal/personal.el
@@ -6,3 +6,15 @@
 
 ;; Auto revert in doc view mode
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
+
+;; Prelude Config
+(defun prelude-prog-mode-hook ()
+ (flyspell-prog-mode)
+ (prelude-local-comment-auto-fill)
+ (prelude-turn-off-whitespace)
+ (prelude-turn-on-abbrev)
+ (prelude-add-watchwords)
+ ;;(linum-mode +1)
+ (auto-fill-mode -1)
+ ;; keep the whitespace decent all the time
+ (add-hook 'before-save-hook 'whitespace-cleanup nil t))
